e278d85c83bd59749d075d180bf5282c4d7d7463
martinlukk <66031887+martinlukk@users.noreply.github.com>
2023-01-30 18:58:57 -0500

    Clean V-Party data

--- 1-clean_merge.R
+++ 1-clean_merge.R
@@ -13,12 +13,14 @@ library(tidyverse)
 library(here)
 library(rio)
 library(manifestoR); mp_setapikey("manifesto_apikey.txt") # NOTE: Obtain API key at https://manifestoproject.wzb.eu/
+library(vdemdata) # NOTE: install with devtools::install_github("vdeminstitute/vdemdata")
 
 
 # 1. Load Data ------------------------------------------------------------
 populist <- import("https://popu-list.org/wp-content/uploads/2020/06/populist-version-2-20200626.xlsx") 
 parlgov  <- read_csv("https://www.parlgov.org/data/parlgov-development_csv-utf-8/view_party.csv")
 cmp      <- mp_maindataset(version = "2022a")
+vparty   <- vdemdata::vparty
 
 
 # 2. Clean and Merge Data -------------------------------------------------
@@ -49,12 +51,42 @@ parlgov <-
   select(id_parlgov, country_name, country_id,
          party_name_english, party_name_short,
          left_right, state_market, liberty_authority, eu_anti_pro)
+vparty <- 
+  vparty %>% 
+  select(v2paenname, v2pashname,
+         pf_party_id, country_name, e_regionpol, year,
+         v2xpa_antiplural, v2xpa_popul, # Note: These variables are indices based on subsequent ones
+         v2paanteli, v2papeople, v2paopresp, v2paplur, v2paminor, v2paviol,
+         v2paimmig, v2palgbt, v2paculsup, v2parelig, v2pagender, v2pawomlab,
+         v2pariglef, v2pawelf, v2paclient,
+         starts_with(c("v2pasalie", "v2pagroup", "v2pafunds")),
+         v2paactcom, v2pasoctie, v2panom, v2padisa, v2paind,
+         ep_antielite_salience, ep_corrupt_salience, ep_members_vs_leadership,
+         ep_people_vs_elite, ep_type_populism, ep_type_populist_values,
+         ep_v8_popul_rhetoric, ep_v9_popul_saliency, ep_galtan, ep_galtan_salience,
+         ep_v6_lib_cons, ep_v7_lib_cons_saliency) %>% 
+  rename(id_partyfacts = pf_party_id) %>% 
+  filter(year >= 1989, !is.na(id_partyfacts)) %>% 
+  arrange(country_name, id_partyfacts, year)
+
+vparty_train <-
+  vparty %>% 
+  filter(country_name %in% populist_countries) %>% 
+  left_join(., populist, by = c("id_partyfacts", "year"), multiple = "first") %>% 
+  relocate(v2paenname, country_name, id_partyfacts, year, populist) %>% 
+  # Code all parties not featured in Popu-List as "not populist"
+  mutate(populist = replace_na(populist, 0))
 
 cmp <- 
   cmp %>% 
   select(countryname, edate, party, partyname, partyabbrev,
          per101:per706) %>% 
   rename(id_manifesto = party)
+vparty_test <- 
+  vparty %>% 
+  filter(!country_name %in% populist_countries) %>% 
+  relocate(v2paenname, country_name, id_partyfacts, year)
+  
 
 # TODO: Figure out how to incorporate time-varying party data from manifestos
 # with time-invariant data from parlgov and populist. Avg manifesto values for
