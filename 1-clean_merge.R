bd999687da3c1c93318b53a3ab2d731a90d80790
martinlukk <66031887+martinlukk@users.noreply.github.com>
2023-01-30 19:00:31 -0500

    Omit parlgov, cmp data cleaning

--- 1-clean_merge.R
+++ 1-clean_merge.R
@@ -12,15 +12,15 @@
 library(tidyverse)
 library(here)
 library(rio)
-library(manifestoR); mp_setapikey("manifesto_apikey.txt") # NOTE: Obtain API key at https://manifestoproject.wzb.eu/
 library(vdemdata) # NOTE: install with devtools::install_github("vdeminstitute/vdemdata")
+# library(manifestoR); mp_setapikey("manifesto_apikey.txt") # NOTE: Obtain API key at https://manifestoproject.wzb.eu/
 
 
 # 1. Load Data ------------------------------------------------------------
 populist <- import("https://popu-list.org/wp-content/uploads/2020/06/populist-version-2-20200626.xlsx") 
-parlgov  <- read_csv("https://www.parlgov.org/data/parlgov-development_csv-utf-8/view_party.csv")
-cmp      <- mp_maindataset(version = "2022a")
 vparty   <- vdemdata::vparty
+# parlgov  <- read_csv("https://www.parlgov.org/data/parlgov-development_csv-utf-8/view_party.csv")
+# cmp      <- mp_maindataset(version = "2022a")
 
 
 # 2. Clean and Merge Data -------------------------------------------------
@@ -41,16 +41,7 @@ populist <-
   mutate(year = map2(populist_start, populist_end, seq)) %>% 
   select(-populist_start, -populist_end) %>% 
   unnest(cols = year)
-  
-  
 
-parlgov <- 
-  parlgov %>%
-  # Harmonize ID variable
-  rename(id_parlgov = party_id) %>% 
-  select(id_parlgov, country_name, country_id,
-         party_name_english, party_name_short,
-         left_right, state_market, liberty_authority, eu_anti_pro)
 vparty <- 
   vparty %>% 
   select(v2paenname, v2pashname,
@@ -77,11 +68,6 @@ vparty_train <-
   # Code all parties not featured in Popu-List as "not populist"
   mutate(populist = replace_na(populist, 0))
 
-cmp <- 
-  cmp %>% 
-  select(countryname, edate, party, partyname, partyabbrev,
-         per101:per706) %>% 
-  rename(id_manifesto = party)
 vparty_test <- 
   vparty %>% 
   filter(!country_name %in% populist_countries) %>% 
@@ -91,11 +77,20 @@ vparty_test <-
 # TODO: Figure out how to incorporate time-varying party data from manifestos
 # with time-invariant data from parlgov and populist. Avg manifesto values for
 # each party within popu-list time period?
+# parlgov <- 
+#   parlgov %>%
+#   # Harmonize ID variable
+#   rename(id_parlgov = party_id) %>% 
+#   select(id_parlgov, country_name, country_id,
+#          party_name_english, party_name_short,
+#          left_right, state_market, liberty_authority, eu_anti_pro)
+# 
+# cmp <- 
+#   cmp %>% 
+#   select(countryname, edate, party, partyname, partyabbrev,
+#          per101:per706) %>% 
+#   rename(id_manifesto = party)
 
-df <- 
-  left_join(parlgov, populist, by = "id_parlgov") %>% 
-  # Create variable indicating if is party not included in Populist
-  mutate(not_in_populist = if_else(is.na(populist), 1, 0))
 
 
 # 3. Save data file -------------------------------------------------------
